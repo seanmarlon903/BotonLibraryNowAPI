d64f858c337bf12aa43370e8aaa5e81f6eea46f6
STUDENT 33 <STUDENT 33@DESKTOP-GJFUHHS>
2026-05-09 13:34:18 +0800

    Add project files.

---
+++ BotonLibraryNowAPI/Controllers/BooksController.cs
@@ -0,0 +1,71 @@
+﻿using BotonLibraryNowAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BotonLibraryNowAPI.Controllers
+{
+    [Route("api/v1/books")]
+    [ApiController]
+
+    public class BooksControllers : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+       {
+           new Book { Id = 1, Title = "Theo Of Golden" , Author = "Allen Levi", Genre = "Contemporary Fiction" ,Available = true, PublishedYear = 2025 },
+           new Book { Id = 2, Title = "Theo " , Author = " Levi", Genre = " Fiction" ,Available = true, PublishedYear = 2020 }
+
+       };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new { status = "sucess", data = books, message = "Books retrieved" });
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+
+                return NotFound(new { status = "error", data = (object?)null, message = "s not found" });
+            return Ok(new { status = "success", data = books, message = "Books Retrieved" });
+
+        }
+        [HttpPost("{id}")]
+        public IActionResult Create([FromBody] Book newbook)
+        {
+            newbook.Id = books.Count + 1;
+            books.Add(newbook);
+            return CreatedAtAction(nameof(GetById), new { id = newbook.Id },
+            new { status = "success", data = books, message = "Books retrieved" });
+        }
+        [HttpPut]
+        public IActionResult Update(int id, [FromBody] Book Updatebook)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+                return NotFound(new { status = "error", data = (object?)null, messagae = "s not found." });
+            book.Title = Updatebook.Title;
+            book.Author = Updatebook.Author;
+            book.Genre = Updatebook.Genre;
+            book.Available = Updatebook.Available;
+            book.PublishedYear = Updatebook.PublishedYear;
+            return Ok(new { status = "success", data = books, message = "Books retrieved" });
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(book => book.Id == id);
+            if (book == null)
+                return NotFound(new { status = "error", data = (object?)null, messagae = "s not found." });
+            books.Remove(book);
+            return Ok(new { status = "success", data = books, message = "Books retrieved" });
+
+
+        }
+    }
+}
+
---
+++ BotonLibraryNowAPI/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"https:");
+
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapControllers();
+
+app.Run();
+
+
+
